37292e7097fb86c3de338abead359449461cdd0c
Rodrigo <rodrigocosta47@outlook.com>
2021-01-07 21:40:14 -0300

    Retirei o colect

--- clientes_admin/settings.py
+++ clientes_admin/settings.py
@@ -112,11 +112,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
